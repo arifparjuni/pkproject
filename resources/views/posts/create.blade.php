d988c00cd7843f71d07eb6f28f52fdc17420089f
Arif Parjuni <arifparjjuni@gmail.com>
2020-06-18 19:57:32 +0700

    semua fungsi upload

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <h1>Posts</h1>
-        {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST']) !!}
+        {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
             <div class="form-group">
                 {{Form::label('title', 'Title')}}
                 {{Form::text('title','',['class' => 'form-control','placeholder' => 'Title'])}}
@@ -11,6 +11,9 @@
             <div class="form-group">
                 {{Form::label('body', 'Body')}}
                 {{Form::textarea('body','',['class' => 'form-control','placeholder' => 'Body'])}}
+            </div>
+            <div class="form-group">
+                {{Form::file('cover_image')}}
             </div>
             {{Form::submit('Submit',['class' => 'btn btn-primary'])}}
         {!! Form::close() !!}
